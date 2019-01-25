30a0b6fa678c50d4f618adb698b837bee087b764
Ztuu <stuartpdev@gmail.com>
2019-01-25 21:41:36 +0000

    Created a Game Controller and main menu

---
+++ Game jam 2019/Assets/Scripts/GameController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public bool gameOn; //Determines whether main game loop is running
+    public MainMenu mainMenu;
+    public static GameController instance; //Singleton
+
+    void Awake()
+    {
+        gameOn = false;
+
+        //Singleton check
+        if(instance != null)
+        {
+            Destroy(this);
+        }else
+        {
+            instance = this;
+        }
+    }
+
+    //Should be called by a UI button on menu, assign in editor
+    public void StartGame()
+    {
+        gameOn = true;
+        mainMenu.disable();
+    }
+
+    void GameOver()
+    {
+        gameOn = false;
+        mainMenu.enable();
+    }
+}
