92ae8edccc0ad74788a5628bfaa51f4b5cf0e58d
mobeenaftab <mobeenaftab1@gmail.com>
2019-01-26 00:49:59 +0000

    Player Movement
    Added two new ways of player movement

---
+++ Game jam 2019/Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ArrowMovement();
+    }
+
+    /// <summary>
+    /// Move the object using forces, the longer the press the greater the force applied
+    /// Must freeze x,y,z ridgidbody rotation for this to work
+    /// </summary>
+    private void ForceMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    /// <summary>
+    /// Move game object based on Transform position
+    /// </summary>
+    private void ArrowMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+}
