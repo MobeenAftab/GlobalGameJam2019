b301aa18c5b8caca7752f6750dcbfa319c6da04b
chenning17 <chenning17@users.noreply.github.com>
2019-01-26 19:03:58 +0000

    added music
    music added

---
+++ Game jam 2019/Assets/MusicPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip intro;
+    public AudioSource source;
+    public AudioClip loop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        source.clip = intro;
+        source.Play();
+      //  intro.Play();
+      //  loop.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (source.isPlaying)
+        {
+
+        }
+        else
+        {
+            source.clip = loop;
+            source.Play();
+        }
+    }
+}
