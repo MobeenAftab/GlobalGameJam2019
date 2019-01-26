26a0ed1e76aa6947d8a3f2a4ba47a1dde8948eff
mobeenaftab <mobeenaftab1@gmail.com>
2019-01-26 22:28:27 +0000

    Added Noise Manager
    Noise manager game object and Noise superclass for all noise objects

---
+++ Game jam 2019/Assets/Scripts/NoiseManager/NoiseManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseManager : MonoBehaviour
+{
+    public float noiseLevel = 0.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("NoiseManager Awake");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateNoise(float updateNoise)
+    {
+        this.noiseLevel += updateNoise;
+        Debug.Log("Noise Manager: " + noiseLevel);
+    }
+}
---
+++ Game jam 2019/Assets/Scripts/NoiseManager/NoiseSuperclass.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Interface that all object that make noise must implement.
+ *  Must implement three properties:
+ *  noise value of object.
+ *  get player object.
+ *  update player object noise level
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseSuperclass : MonoBehaviour
+{
+    public float NoiseInc { get; set; } = 5.0f;
+    private bool isCollision = false;
+
+    public NoiseManager noiseM;
+
+    void Start()
+    {
+        noiseM = GameObject.Find("NoiseManager").GetComponent<NoiseManager>();
+        Debug.Log("YEYEY: " + noiseM.noiseLevel);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        noiseM.UpdateNoise(NoiseInc);
+        Debug.Log("NoiseTest" + collision + "\t" + collision.gameObject.tag);
+        Debug.Log("Collision Position: " + collision.transform.position);
+    }
+
+    /// <summary>
+    /// Reduce the noise level every second or so
+    /// </summary>
+    void FixedUpdate()
+    {
+    }
+
+
+
+
+}
