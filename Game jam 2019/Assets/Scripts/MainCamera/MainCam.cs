2a749c8652b5bcdb6647ab9306e0d456641a4f4d
Ztuu <stuartpdev@gmail.com>
2019-01-26 16:58:48 +0000

    Tweaked camera angle
    Camera now angled down on player from higher up

--- Game jam 2019/Assets/Scripts/MainCamera/MainCam.cs
+++ Game jam 2019/Assets/Scripts/MainCamera/MainCam.cs
@@ -9,6 +9,7 @@ public class MainCam : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.forward = -1 * playerPos.right;
-        transform.position = playerPos.position + playerPos.right * 5.0f + playerPos.up * 3.0f;
+        transform.position = playerPos.position + playerPos.right * 5.0f + playerPos.up * 5.0f;
+        transform.Rotate(new Vector3(1.0f, 0.0f, 0.0f), 30.0f);
     }
 }
